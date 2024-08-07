08db30dffba110649097b2e74f4b325164321bab
Filippo Lauria <filippo.lauria@iit.cnr.it>
2024-08-07 10:29:15 +0200

    improved and simplified start_workers

--- core/workers.py
+++ core/workers.py
@@ -1,22 +1,16 @@
 import threading
-
-from bin.scanner    import scanner
-from bin.attacker   import attacker
-from bin.scheduler  import scheduler
+from bin.scanner import scanner
+from bin.attacker import attacker
+from bin.scheduler import scheduler
 
 
 def start_workers():
-  thread = threading.Thread(target=scanner)
-  thread.name = "scanner"
-  thread.daemon = True
-  thread.start()
+    workers = [
+        ("scanner", scanner),
+        ("attacker", attacker),
+        ("scheduler", scheduler)
+    ]
 
-  thread = threading.Thread(target=attacker)
-  thread.name = "attacker"
-  thread.daemon = True
-  thread.start()
-
-  thread = threading.Thread(target=scheduler)
-  thread.name = "scheduler"
-  thread.daemon = True
-  thread.start()
+    for name, target in workers:
+        thread = threading.Thread(target=target, name=name, daemon=True)
+        thread.start()
