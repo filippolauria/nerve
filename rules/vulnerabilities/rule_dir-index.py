947e12b7d89c2aec659ff4f2dbafa577857d9e40
filippolauria <filippo.lauria@iit.cnr.it>
2024-08-13 17:54:26 +0200

    Improved repo discovery rules

--- rules/vulnerabilities/rule_dir-index.py
+++ rules/vulnerabilities/rule_dir-index.py
@@ -1,48 +1,39 @@
-from core.redis   import rds
-from core.triage  import Triage
-from core.parser  import ScanParser
-from db.db_paths  import COMMON_WEB_PATHS
+from core.parser import ScanParser
+from core.redis import rds
+from core.rules import BaseRule
+from core.triage import Triage
+from db.db_paths import COMMON_WEB_PATHS
 
-class Rule:
-  def __init__(self):
-    self.rule = 'VLN_Z013'
-    self.rule_severity = 4
-    self.rule_description = 'This rule checks for Open Directories via Directory Indexing'
-    self.rule_confirm = 'Remote Server has Directory Indexing Enabled'
-    self.rule_details = ''
-    self.rule_mitigation = '''Disable Directory Indexing on the server. Directory Indexing can allow access to files on the server to untrusted sources.'''
-    self.uris = COMMON_WEB_PATHS
-    self.intensity = 3
 
-  def check_rule(self, ip, port, values, conf):
-    t = Triage()
-    p = ScanParser(port, values)
-    
-    module = p.get_module()
-    domain = p.get_domain()
+class Rule(BaseRule):
+    def __init__(self):
+        super().__init__()
+        self.rule = 'VLN_Z013'
+        self.rule_severity = 4
+        self.rule_description = 'This rule checks for Open Directories via Directory Indexing'
+        self.rule_confirm = 'Remote Server has Directory Indexing Enabled'
+        self.rule_details = ''
+        self.rule_mitigation = (
+            'Disable Directory Indexing on the server. '
+            'Directory Indexing can allow access to files on the server to untrusted sources.'
+        )
+        self.intensity = 3
+        self.matches = ['C=N;O=D', 'Index of /', ]
 
-    if 'http' not in module: 
-      return
+    def check_rule(self, ip, port, values, conf):
+        scan_parser = ScanParser(port, values)
 
-    resp = None
+        if not scan_parser.is_module('http'):
+            return
 
-    for uri in self.uris:
-      resp = t.http_request(ip, port, uri=uri)
-      if resp:
-        for match in ('C=N;O=D', 'Index of /'):
-          if match in resp.text:
-            self.rule_details = 'Identified an Open Directory at {}'.format(resp.url)
-            rds.store_vuln({
-              'ip':ip,
-              'port':port,
-              'domain':domain,
-              'rule_id':self.rule,
-              'rule_sev':self.rule_severity,
-              'rule_desc':self.rule_description,
-              'rule_confirm':self.rule_confirm,
-              'rule_details':self.rule_details,
-              'rule_mitigation':self.rule_mitigation
-            })
-            break
-    
-    return
+        triage = Triage()
+
+        for uri in COMMON_WEB_PATHS:
+            response = triage.http_request(ip, port, uri=uri)
+            if not response or not self.check_match(response, self.matches):
+                continue
+
+            self.rule_details = f'Identified an Open Directory at {response.url}'
+            domain = scan_parser.get_domain()
+            vuln_dict = self.get_vuln_dict(ip, port, domain)
+            rds.store_vuln(vuln_dict)
--- rules/vulnerabilities/rule_svn-repo.py
+++ rules/vulnerabilities/rule_svn-repo.py
@@ -1,43 +1,35 @@
-from core.redis  import rds
-from core.triage import Triage
 from core.parser import ScanParser
-from core.utils  import Utils
+from core.redis import rds
+from core.rules import BaseRule
+from core.triage import Triage
+from db.db_paths import COMMON_WEB_PATHS
 
-class Rule:
-  def __init__(self):
-    self.rule = 'VLN_BLKK'
-    self.rule_severity = 4
-    self.rule_description = 'This rule checks for open SVN Repositories'
-    self.rule_confirm = 'SVN Repository Found'
-    self.rule_details = ''
-    self.rule_mitigation = '''Block remote access to the Subversion repository.'''
-    self.intensity = 0
-                                    
-  def check_rule(self, ip, port, values, conf):
-    t = Triage()
-    p = ScanParser(port, values)
-    
-    domain = p.get_domain()
-    module = p.get_module()
-    
-    if 'http' not in module:
-      return
-    
-    resp = t.http_request(ip, port, uri='/.svn/text-base')
 
-    if resp and 'Index of /' in resp.text:
-      self.rule_details = 'SVN Repository exposed at {}'.format(resp.url)
-      rds.store_vuln({
-        'ip':ip,
-        'port':port,
-        'domain':domain,
-        'rule_id':self.rule,
-        'rule_sev':self.rule_severity,
-        'rule_desc':self.rule_description,
-        'rule_confirm':self.rule_confirm,
-        'rule_details':self.rule_details,
-        'rule_mitigation':self.rule_mitigation
-      })
-    
-    return
+class Rule(BaseRule):
+    def __init__(self):
+        super().__init__()
+        self.rule = 'VLN_BLKK'
+        self.rule_severity = 4
+        self.rule_description = 'This rule checks for open SVN Repositories'
+        self.rule_confirm = 'SVN Repository Found'
+        self.rule_details = ''
+        self.rule_mitigation = '''Block remote access to the Subversion repository.'''
+        self.intensity = 0
 
+    def check_rule(self, ip, port, values, conf):
+        scan_parser = ScanParser(port, values)
+
+        if not scan_parser.is_module('http'):
+            return
+
+        triage = Triage()
+
+        for uri in COMMON_WEB_PATHS:
+            response = triage.http_request(ip, port, uri=f"{uri}/.svn/text-base")
+            if not response or 'Index of /' not in response.text:
+                continue
+
+            self.rule_details = f'Identified a SVN repository at {response.url}'
+            domain = scan_parser.get_domain()
+            vuln_dict = self.get_vuln_dict(ip, port, domain)
+            rds.store_vuln(vuln_dict)
